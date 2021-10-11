fe630794da158ea0d938376797d0b52e3eb1a2f7
Tan Tran <tantran1610@gmail.com>
2021-10-11 18:48:47 +0700

    nop bai tap 1 Rn

--- src/Buoi1/DemoStyle/index.js
+++ src/Buoi1/DemoStyle/index.js
@@ -6,7 +6,7 @@ export default function DemoStyle() {
         <View style={{ marginTop: 50, paddingLeft: 10, paddingRight: 20 }}>
             <View style={{
                 backgroundColor: '#3D85C6',
-                height: 100,
+                height: 145,
                 justifyContent: 'center',
                 alignItems: 'center',
                 borderRadius: 10,
@@ -16,7 +16,7 @@ export default function DemoStyle() {
             </View>
             <View style={{
                 backgroundColor: '#C9C9FF',
-                height: 100,
+                height: 145,
                 justifyContent: 'center',
                 alignItems: 'center',
                 borderRadius: 10,
@@ -24,6 +24,37 @@ export default function DemoStyle() {
             }}>
                 <Text>Purple : #C9C9FF</Text>
             </View>
+            <View style={{
+                backgroundColor: '#FF0000',
+                height: 145,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 10,
+                marginBottom: 10
+            }}>
+                <Text>Red : ##FF0000</Text>
+            </View>
+            <View style={{
+                backgroundColor: '#FFFF00',
+                height: 145,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 10,
+                marginBottom: 10
+            }}>
+                <Text>Yellow : #FFFF00</Text>
+            </View>
+            <View style={{
+                backgroundColor: '#FFC0CB',
+                height: 145,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 10,
+                marginBottom: 10
+            }}>
+                <Text>Pink : #FFC0CB</Text>
+            </View>
+
         </View>
     )
 }
